13990def716a94889aece72ad9ba25178c9cf8bc
kevin <kevin@squirrel.fr>
2021-11-04 00:55:37 +0400

    GetTasksById
    - set controller
    - set service

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Get, Body } from '@nestjs/common';
+import { Controller, Post, Get, Body, Param } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto'
 import { Task } from './task.models';
 import { TasksService } from './tasks.service';
@@ -10,6 +10,11 @@ export class TasksController {
     @Get()
     getAllTasks(): Task[] {
         return this.tasksService.getAllTasks()
+    }
+
+    @Get('/:id')
+    getTasksById(@Param('id') id: string): Task {
+        return this.tasksService.getTasksById(id);
     }
 
     @Post()
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -11,6 +11,10 @@ export class TasksService {
         return this.tasks;
     }
 
+    getTasksById(id: string): Task {
+        return this.tasks.find(task => task.id === id);
+    }
+
     createTask(createTaskDto: CreateTaskDto): Task {
         const { title, description } = createTaskDto;
         
